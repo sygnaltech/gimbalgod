910cd242114b0bfc39b6cd07fe75effc584544e1
Michael Wells <info@sygnal.com>
2023-09-24 02:17:22 +0000

    Updated

--- netlify/functions/youtube.js
+++ netlify/functions/youtube.js
@@ -1,5 +1,12 @@
 const fetch = require('node-fetch');
-const YOUTUBE_API_URL = "YOUR_YOUTUBE_API_URL";  // make sure to define this
+
+
+const PLAYLIST_ID = 'PLx0sYbCqOb8TBPRdmBHs5Iftvv9TPboYG';
+const API_KEY = 'AIzaSyDvg64eKENjQGovEYQYkX-3wAai3_XGy6s';
+// const ACCESS_TOKEN = '[YOUR_ACCESS_TOKEN]';
+const YOUTUBE_API_URL = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails,snippet&maxResults=50&playlistId=${PLAYLIST_ID}&key=${API_KEY}`;
+
+//const YOUTUBE_API_URL = "YOUR_YOUTUBE_API_URL";  // make sure to define this
 
 exports.handler = async function(event, context) {
     try {
